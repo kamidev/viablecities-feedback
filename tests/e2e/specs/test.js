b6e045b6a95a66e64b917b548ec1f8c4d31c62f9
kamidev <jonas.beckman@gmail.com>
2018-08-22 13:05:22 +0200

    Make test work for initial Auth0 screen

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -5,10 +5,7 @@ module.exports = {
   "default e2e tests": browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible("#app", 5000)
-      .assert.elementPresent(".hello")
-      .assert.containsText("h1", "Welcome to Your Vue.js App")
-      .assert.elementCount("img", 1)
+      .waitForElementVisible("#auth0-lock-container-1", 5000)
       .end();
   }
 };
