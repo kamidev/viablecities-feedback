ebeea8c5ed492bac1727b16e3030c3fb4fe99ccb
kamidev <jonas.beckman@gmail.com>
2018-08-22 13:13:29 +0200

    Disable two eslint warnings

--- src/auth.js
+++ src/auth.js
@@ -54,6 +54,7 @@ let auth = new Vue({
       webAuth.authorize();
     },
     logout() {
+      // eslint-disable-next-line
       return new Promise((resolve, reject) => {
         localStorage.removeItem("access_token");
         localStorage.removeItem("id_token");
--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -6,6 +6,7 @@
 export default {
   name: "callback",
   mounted() {
+    // eslint-disable-next-line
     this.$auth.handleAuthentication().then(data => {
       this.$router.push({ name: "home" });
     });
