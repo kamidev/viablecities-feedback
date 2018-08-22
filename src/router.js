72cdab5dd584bd325b8dd7a7631c9534a038491b
kamidev <jonas.beckman@gmail.com>
2018-08-22 12:58:10 +0200

    WIP - working viable code

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "@/views/Home.vue";
+import Callback from "@/views/Callback.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
-  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -14,13 +14,25 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/callback",
+      name: "callback",
+      component: Callback
     }
   ]
 });
+
+// very basic "setup" of a global guard
+router.beforeEach((to, from, next) => {
+  if (to.name == "callback") {
+    // check if "to"-route is "callback" and allow access
+    next();
+  } else if (router.app.$auth.isAuthenticated()) {
+    // if authenticated allow access
+    next();
+  } else {
+    // trigger auth0 login
+    router.app.$auth.login();
+  }
+});
+
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+        <nav class="navbar navbar-light bg-light">
+     <img src="../assets/logo.png">
+      <div>
+        <img :src="$auth.user.picture" width="30" height="30">
+        <span class="text-muted font-weight-light px-2">{{$auth.user.name}}</span>
+        <button type="button" class="btn btn-outline-secondary btn-sm" @click="$auth.logout()">Logout</button>
+      </div>
+    </nav>
+    <survey :survey="survey" /> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from "vue";
+import * as SurveyVue from "survey-vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "bootstrap/dist/css/bootstrap.css";
+
+Vue.use(VueAxios, axios);
+
+var Survey = SurveyVue.Survey;
+Survey.cssType = "bootstrap";
 
 export default {
-  name: "home",
+  name: "app",
   components: {
-    HelloWorld
+    Survey
+  },
+  data() {
+    var json = {
+      title: "Ingen uppföljningsmall är tillgänglig för närvarande."
+    };
+    var model = new SurveyVue.Model(json);
+    return {
+      survey: model
+    };
+  },
+  // Add event hooks: https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+  // See: https://vuejs.org/v2/api/#mounted
+  mounted() {
+    // Get specified survey template from external API
+    Vue.axios.get("https://localhost:4001/api/surveys/3").then(
+      /*     Vue.axios
+      .get("https://sharingcities.evothings.com/survey_api/surveys/3")
+      .then( */
+      result => {
+        var survey_id = result.data.data.survey_id;
+        var template = result.data.data.survey_design;
+        var new_model = new SurveyVue.Model(template);
+        // Add model handler to save completed survey
+        new_model.onComplete.add(function(result) {
+          var answers = JSON.stringify(result.data);
+          var pseudonym = "anonymous";
+          if (result.data.want_followup) {
+            pseudonym = result.pseudonym;
+          }
+          // eslint-disable-next-line
+          console.log("Saved survey results: " + answers);
+          var input = {
+            answer: {
+              survey_id: survey_id,
+              survey_answers: result.data,
+              pseudonym: pseudonym,
+              user_id: 0 // TODO: add real user data here
+            }
+          };
+          axios.post("https://localhost:4001/api/answers", input).then(
+            /* axios
+              .post(
+                "https://sharingcities.evothings.com/survey_api/answers",
+                input
+              )
+              .then( */
+            // eslint-disable-next-line
+            result => {
+              this.response = answers;
+            },
+            error => {
+              // eslint-disable-next-line
+              console.error(error);
+            }
+          );
+        });
+        // Update empty survey with model loaded from API
+        Vue.set(this, "survey", new_model);
+      },
+      error => {
+        // eslint-disable-next-line
+        console.error(error);
+      }
+    );
   }
 };
 </script>
