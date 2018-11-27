e62b4c321309b625d6e1821fee5c2afbd6780e62
kamidev <jonas.beckman@gmail.com>
2018-11-27 20:59:45 +0100

    Add fixed audience setting

--- src/auth.js
+++ src/auth.js
@@ -9,7 +9,7 @@ let webAuth = new auth0.WebAuth({
   redirectUri: "http://localhost:8080/callback",
   //redirectUri: "https://sharingcities.evothings.com/viable/callback",
   // we will use the api/v2/ to access the user information as payload
-  // audience: "https://kamidev.eu.auth0.com/userinfo",
+  audience: "https://sharingcities.evothings.com/viable/survey_api",
   responseType: "token id_token",
   scope: "openid profile"
 });
